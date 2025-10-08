6757bf98a53d4e41f29551e48127b340413db13f
shule517 <shule517@gmail.com>
2025-10-09 00:32:31 +0900

    シーンの追加

--- player/Player.cs
+++ player/Player.cs
@@ -3,4 +3,11 @@ using System;
 
 public partial class Player : Node2D
 {
+    [Export] public float Size = 16;
+    [Export] public Color Color = new Color(0.9f, 0.2f, 0.2f);
+
+    public override void _Draw()
+    {
+        DrawRect(new Rect2(Vector2.Zero, new Vector2(Size, Size)), Color);
+    }
 }
