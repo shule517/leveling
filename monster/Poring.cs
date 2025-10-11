304bb08aae4f288ac157f20ed0d4ddfc466ad8c8
shule517 <shule517@gmail.com>
2025-10-11 15:00:28 +0900

    モンスターの視野を追加

--- monster/Poring.cs
+++ monster/Poring.cs
@@ -9,4 +9,14 @@ public partial class Poring : CharacterBody2D
     public override void _Draw() => DrawCircle(Vector2.Zero, _size / 2, _color);
 
     [Node] private Area2D _visionArea2D = null!;
+
+    private void _on_vision_area_2d_body_entered(Node2D body)
+    {
+        if (body.IsInGroup("Player"))
+        {
+            var player = body as Player;
+            Position = player.Position;
+            GD.Print("Playerが視野に入りました！");
+        }
+    }
 }
