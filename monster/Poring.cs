e642bafba082b7f940c8aea887c961a4e2c783d1
shule517 <shule517@gmail.com>
2025-10-11 14:10:27 +0900

    モンスターを追加した

---
+++ monster/Poring.cs
@@ -0,0 +1,10 @@
+using Godot;
+using System;
+
+[Tool]
+public partial class Poring : CharacterBody2D
+{
+    private float _size = 16;
+    private Color _color = new Color("f27d73");
+    public override void _Draw() => DrawCircle(Vector2.Zero, _size / 2, _color);
+}
--- player/Player.cs
+++ player/Player.cs
@@ -6,7 +6,6 @@ public partial class Player : CharacterBody2D
 {
     private float _size = 16;
     private Color _color = new Color(0.9f, 0.2f, 0.2f);
-
     public override void _Draw() => DrawRect(new Rect2(Vector2.Zero, new Vector2(_size, _size)), _color, filled: false);
 
     [Export] public float Speed = 200f; // 移動速度
